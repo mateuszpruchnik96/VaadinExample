fc70949c6e179b1560dacd58b0b7320d55106d92
Maciej.Koryl <maciej.koryl@asseco.pl>
2021-05-07 17:15:53 +0200

    Drobne zmiany dotyczące ServiceRequest

--- src/main/java/com/wsiiz/repairshop/servicing/domain/servicerequest/ServiceRequestRepository.java
+++ src/main/java/com/wsiiz/repairshop/servicing/domain/servicerequest/ServiceRequestRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, Long> {
 
+  List<ServiceRequest> findByVehicleId(Long vehicleId);
+
   @Query(value = "select sr from ServiceRequest sr where (:vehicleId = null or sr.vehicleId = :vehicleId) "
       + "and (:requestType = null or sr.requestType = :requestType)")
   List<ServiceRequest> findBySelectionCriteria(@Param("vehicleId") Long vehicleId,
